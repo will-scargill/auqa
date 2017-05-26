51ff91b94c42c6f218739bd1dcb98f5d5aa5257b
Will Scargill <willscargill@outlook.com>
2017-05-26 13:04:46 +0100

    Updated index.py

--- dashboard/index.py
+++ dashboard/index.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello World!"
+  
+@app.route("/infomation")
+def infomation():
+    return "This is a website for the plant system we are making"
 
 if __name__ == "__main__":
     app.run()
