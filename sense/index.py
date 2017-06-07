df78ff90e6307cbdf9d6902a3b243e85a9cae842
Jake Walker <jcbod101@gmail.com>
2017-06-07 17:46:56 +0100

    Added more tests for sense

--- sense/index.py
+++ sense/index.py
@@ -1,21 +1,30 @@
-
-
 # main file for collecting data from sensors
-from sense_hat import SenseHat
+import os
+
+os.environ["TEST"] = "y"
+
+if os.environ["TEST"] != "y": # Make sure we are not testing as this will import error
+    from sense_hat import SenseHat
 import sqlite3
 import time
 
-
-conn = sqlite3.connect("../database.db") #SQLite3 Setup
+conn = sqlite3.connect("database.db") #SQLite3 Setup
 c = conn.cursor() #SQL Cursor
 
-c.execute("DROP TABLE data")
+c.execute("""CREATE TABLE IF NOT EXISTS `data` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`timestamp`	DATE NOT NULL DEFAULT (datetime('now','localtime')),
+	`humidity`	REAL,
+	`temperature`	REAL,
+	`pressure`	REAL
+);""")
 
-c.execute("CREATE TABLE IF NOT EXISTS data (date STRING, time STRING, humidity STRING, temp STRING, pressure STRING)")
+conn.commit()
 
-s = SenseHat() #Sensehat Setup
-
-
+if os.environ["TEST"] != "y":
+    s = SenseHat() #Sensehat Setup
+else:
+    s = None
 
 def get_data(): #Get humidity and temperature
     humidity = s.get_humidity()
@@ -25,18 +34,16 @@ def get_data(): #Get humidity and temperature
 Current Humidity: """ + str(humidity) + """
 Current Temperature: """ + str(temp) + """
 Current Air Pressure: """ + str(pressure))
-    date = time.strftime("%Y-%m-%d", time.gmtime())
-    ctime = time.strftime("%H:%M", time.gmtime())
-    print("Date: " + str(date))
-    print("Time: " + str(ctime))
 
-    c.execute("INSERT INTO data (date, time, humidity, temp, pressure) VALUES(?, ?, ?, ?, ?)", [date, ctime, humidity, temp, pressure])
+    insert_data(humidity, temp, pressure)
+
+def insert_data(humidity, temperature, pressure): # Put data into the database
+    c.execute("INSERT INTO `data` (humidity, temperature, pressure) VALUES (?, ?, ?)", [humidity, temperature, pressure])
     conn.commit()
-    
+    return c.lastrowid
 
-
-while True:
-    get_data()
-    time.sleep(10)
-
- 
+# TODO: Replace this with a CRON Job
+if os.environ["TEST"] != "y": # Only run this if we aren't doing tests
+    while True:
+        get_data()
+        time.sleep(10)
